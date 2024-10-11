370aadaf104128b0a9411756875261e4bc72d242
BlackfireZZZ <Stipundrik@gmail.com>
2024-10-11 18:29:32 +0300

    Update carpet

--- Carpet.py
+++ Carpet.py
@@ -1,6 +1,23 @@
-class Carpet(object):
-    def __init__(self, id: int):
+import json
+
+class Action(object):
+    acceleration = [0, 0]
+    activateShield = False
+    attack = [0, 0]
+
+    def __init__(self, id: str):
         self.id = id
+
+
+class Carpet(object):
+    def __init__(self, id: str):
+        self.id = id
+        self.action = Action(id)
+
+    def action_to_dict(self):
+        return
+
+
 
     def shoot(self, x: int, y: int, velocity_x: int, velocity_y: int):
         pass

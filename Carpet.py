4579d17618a93076ab522d15c389895d2c496bd7
BlackfireZZZ <Stipundrik@gmail.com>
2024-10-11 18:25:13 +0300

    Carpet class

---
+++ Carpet.py
@@ -0,0 +1,16 @@
+class Carpet(object):
+    def __init__(self, id: int):
+        self.id = id
+
+    def shoot(self, x: int, y: int, velocity_x: int, velocity_y: int):
+        pass
+
+    def scan(self):
+        pass
+
+    def move(self, x: int, y: int):
+        pass
+
+
+
+

6acc909d1ff5d8c4c3a7ec559f9978c71863c776
andrew <chistyakgg@gmail.com>
2024-10-11 18:46:23 +0300

    (school)shooting

--- Carpet.py
+++ Carpet.py
@@ -13,14 +13,28 @@ class Carpet(object):
     def __init__(self, id: str):
         self.id = id
         self.action = Action(id)
+        self.shoot_cd = 0
 
     def action_to_dict(self):
-        return
+        return {
+            "acceleration": {
+                "x": 1.2,
+                "y": 1.2
+            },
+            "activateShield": true,
+            "attack": {
+                "x": 1,
+                "y": 1
+            }
+        }
 
 
 
-    def shoot(self, x: int, y: int, velocity_x: int, velocity_y: int):
-        pass
+    def shoot(self, enemy_x: int, enemy_y: int, velocity_x: int, velocity_y: int):
+        if self.shoot_cd == 0:
+            self.action.attack = [enemy_x + (velocity_x * 0.3), enemy_y + (velocity_y * 0.3)]
+        else:
+            pass
 
     def scan(self):
         pass
